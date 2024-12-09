81ef1ecbe965a0c6a34cc975ce5eeaea21bc15ef
Christian <imchrisa0.5@gmail.com>
2024-12-09 12:12:53 -0500

    Added example to show referencing an object in another object

--- In Class Code/Day 27, Final Review.py
+++ In Class Code/Day 27, Final Review.py
@@ -1,3 +1,4 @@
+'''
 class Cake:
     def __init__(self, flavor, size, color):
         self.flavor = flavor
@@ -23,3 +24,31 @@ def main():
     c2 = eval(c1.__repr__())
     print(c2)
 main()
+'''
+class Cake:
+    def __init__(self, flavor, size, color, cupcake=None):
+        self.flavor = flavor
+        self.size = size
+        self.color = color
+        self.cupcake = cupcake
+
+    def setCupcake(self, cc):
+        self.cupcake = cc
+    
+    def __str__(self):
+        return f"This is a cake that is: {self.size} inches and is {self.flavor}" + \
+        f' and has {self.cupcake} in it.'
+    # Priority is human readable and usually provides information about the data of the object
+    
+    def __repr__(self): #The goal is to a return a string that looks like a call to the object constructor
+        return f'Cake("{self.flavor}", {self.size}, "{self.color}")'
+class Cupcake:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f'{self.name}'
+
+cake1 = Cake("Peach", 12, "orange")
+cup1 = Cupcake("Mike")
+cake1.setCupcake(cup1) #Puts cupcake inside of cake
+print(cake1)
