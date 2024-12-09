36388c0107ea1cb5ccb1b790872a0c9f5cfb9adb
Christian <imchrisa0.5@gmail.com>
2024-12-09 00:49:19 -0500

    word counter complete

--- Misc/word counter.py
+++ Misc/word counter.py
@@ -1,3 +1,7 @@
 def word_counter(text):
     words = text.split #creates a list of all the words; spliting the elements by the space character
-    
+    word_count = 0
+    for word in words:
+        if word.isalpha():
+            word_count += 1
+    return word_count
